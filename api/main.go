b28e7e20792d7866e6838e4c3fd46d191f78dc5e
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-31 15:55:12 -0400

    refactor: print function

--- api/main.go
+++ api/main.go
@@ -11,9 +11,8 @@ import (
 func main() {
 	//obtendo variavel API_PORT
 	config.Load()
-	fmt.Println("Server running...")
 	r := router.Gen()
-
+	fmt.Printf("Server running at port:%d...", config.ApiPort)
 	//definições do servidor
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.ApiPort), r))
 }
