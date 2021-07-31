f1a73a624beaf8a065aa1e9ba6ba7319b09c8b76
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-31 13:19:29 -0400

    feat: routes definition

---
+++ api/src/router/routes/routes.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//definição e representação da estrutura das rotas
+type Route struct {
+	URI            string
+	Method         string
+	Function       func(w http.ResponseWriter, r *http.Request)
+	Authentication bool
+}
+
+// Authentication: requer autenticação?
+
+//funcao que retorna router com rotas já configuradas
+func Config(r *mux.Router) *mux.Router {
+	//carregar as rotas de usuarios
+	routes := userRoutes
+	//usar handlefunc para configurar cada rota
+	for _, route := range routes {
+		//para cada rota em routes crie:
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+	return r
+}
