d471276b6e7a5ac7be20ea81b17ce6849253c714
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-31 13:17:38 -0400

    feat: server definition

--- api/main.go
+++ api/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Server running...")
+	r := router.Gen()
+
+	//definições do servidor
+	log.Fatal(http.ListenAndServe(":5000", r))
 }
