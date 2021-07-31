f882724410b595d5822c07a1b03b958cd31fd59e
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-31 17:54:01 -0400

    feat: user input data treatment

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -4,10 +4,9 @@ import (
 	"api/src/database"
 	"api/src/models"
 	"api/src/repositories"
+	"api/src/responses"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
@@ -16,27 +15,41 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	//lendo corpo da requisição
 	bodyContent, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		//devolver error status entidade improcessavel
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
 	}
 	//inserir body na estrutura de usuário
 	var user models.User
 	if err = json.Unmarshal(bodyContent, &user); err != nil {
-		log.Fatal(err)
+		//devolver um bad request
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+	//verifica se dados de entrada são válidos
+	if err := user.Prepare(); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
 	}
 	//abrir conexão com banco
 	db, err := database.Connect()
 	if err != nil {
-		log.Fatal(err)
+		//devolver internal error
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
 	}
 	defer db.Close()
 	//apos aberta a con com banco, criar repo
 	repo := repositories.NewUserRepository(db)
 	//agora podemos chamar um metodo da struct dentro de repo
-	userID, err := repo.Create(user)
+	user.ID, err = repo.Create(user)
 	if err != nil {
-		log.Fatal()
+		//devolver internal error
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
 	}
-	w.Write([]byte(fmt.Sprintf("User inserted ID:%d", userID)))
+	responses.Json(w, http.StatusCreated, user)
+	//w.Write([]byte(fmt.Sprintf("User inserted ID:%d", userID)))
 }
 
 //Get all User on DB
