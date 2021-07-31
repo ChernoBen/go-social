6047bbc167d2bc25b839936d3527c31c4036eb3c
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-31 15:55:35 -0400

    feat: create user

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -1,10 +1,41 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 //Insert an User in DB
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Creating an User"))
+	//lendo corpo da requisição
+	bodyContent, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//inserir body na estrutura de usuário
+	var user models.User
+	if err = json.Unmarshal(bodyContent, &user); err != nil {
+		log.Fatal(err)
+	}
+	//abrir conexão com banco
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//apos aberta a con com banco, criar repo
+	repo := repositories.NewUserRepository(db)
+	//agora podemos chamar um metodo da struct dentro de repo
+	userID, err := repo.Create(user)
+	if err != nil {
+		log.Fatal()
+	}
+	w.Write([]byte(fmt.Sprintf("User inserted ID:%d", userID)))
 }
 
 //Get all User on DB
