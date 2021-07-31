37d52ad0f9fbc2b7bc175622f82d3a8d911d9fe6
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-31 13:19:55 -0400

    feat:user routes definitions

---
+++ api/src/router/routes/users.go
@@ -0,0 +1,37 @@
+package routes
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+//definição do slice contendo/representado todas as rotas de user
+var userRoutes = []Route{
+	{
+		URI:            "/users",
+		Method:         http.MethodPost,
+		Function:       controllers.CreateUser,
+		Authentication: false,
+	},
+	{
+		URI:            "/users",
+		Method:         http.MethodGet,
+		Function:       controllers.GetUsers,
+		Authentication: false,
+	}, {
+		URI:            "/users/{id}",
+		Method:         http.MethodGet,
+		Function:       controllers.GetUserById,
+		Authentication: false,
+	}, {
+		URI:            "/users/{id}",
+		Method:         http.MethodPut,
+		Function:       controllers.UpdateUser,
+		Authentication: false,
+	}, {
+		URI:            "/users/{id}",
+		Method:         http.MethodDelete,
+		Function:       controllers.DeleteUser,
+		Authentication: false,
+	},
+}
