ccb589a5154ce143cb0e1e650f8b53e764005209
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-07-31 14:34:24 -0400

    feat: user model

---
+++ api/src/models/User.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+//estrutura representado tabela de usuarios com campos mapeados em json
+type User struct {
+	ID        uint64    `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Nick      string    `json:"nick,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"createdat,omitempty"`
+}
+
+//omitempty = omitir se vazio
