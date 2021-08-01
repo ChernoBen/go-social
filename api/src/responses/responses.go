aa0c80b8e3079a9d7fa212ad4ed4456be3ed5be5
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-08-01 14:39:08 -0400

    fix: no content response verify

--- api/src/responses/responses.go
+++ api/src/responses/responses.go
@@ -12,10 +12,14 @@ func Json(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	//passar o status code na response
 	w.WriteHeader(statusCode)
-	//criar json contendo as informações
-	if err := json.NewEncoder(w).Encode(data); err != nil {
-		log.Fatal(err)
+	//verificar se data pertence a um tipo de response 'no content'
+	if data != nil {
+		//criar json contendo as informações
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			log.Fatal(err)
+		}
 	}
+
 }
 
 //função que retornar respostas em formato json para casos de erros
