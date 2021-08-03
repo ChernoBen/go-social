b672493bb9c08a3aff8768bbf29ef847b92c3826
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-08-03 15:19:58 -0400

    fix: correct type for fields created at and nick

--- api/src/models/Articles.go
+++ api/src/models/Articles.go
@@ -3,17 +3,18 @@ package models
 import (
 	"errors"
 	"strings"
+	"time"
 )
 
 //representação da tabela Articles que guarda publicações de usuarios
 type Articles struct {
-	ID         uint64 `json:"id,omitempty"`
-	Title      string `json:"title,omitempty"`
-	Content    string `json:"content,omitempty"`
-	AuthorID   uint64 `json:"author_id,omitempty"`
-	AuthorNick uint64 `json:"author_nick,omitempty"`
-	Likes      uint64 `json:"likes"`
-	CreatedAt  uint64 `json:"createdat,omitempty"`
+	ID         uint64    `json:"id,omitempty"`
+	Title      string    `json:"title,omitempty"`
+	Content    string    `json:"content,omitempty"`
+	AuthorID   uint64    `json:"author_id,omitempty"`
+	AuthorNick string    `json:"author_nick,omitempty"`
+	Likes      uint64    `json:"likes"`
+	CreatedAt  time.Time `json:"createdat,omitempty"`
 }
 
 //metodo que verifica e prepara dados da entrada da requisição
