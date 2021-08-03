963f7058dffe809ba559c021f5191ec933899282
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-08-03 14:21:11 -0400

    feat: article model

--- api/src/models/Articles.go
+++ api/src/models/Articles.go
@@ -3,7 +3,7 @@ package models
 //representação da tabela Articles que guarda publicações de usuarios
 type Articles struct {
 	ID         uint64 `json:"id,omitempty"`
-	Title      uint64 `json:"title,omitempty"`
+	Title      string `json:"title,omitempty"`
 	Content    string `json:"content,omitempty"`
 	AuthorID   uint64 `json:"author_id,omitempty"`
 	AuthorNick uint64 `json:"author_nick,omitempty"`
